0a09a4242fb249fe3eeb1d8ae9ecd0ca18f096bc
Harvey Yerik <yerik05vh@gmail.com>
2022-06-12 11:18:57 -0500

    change success

--- src/soon.js
+++ src/soon.js
@@ -1,11 +1,11 @@
-export default function soonClick(){
-  const settingPomo = document.querySelector(".Setting-pomo")
-  const soon = document.querySelector(".soon-pomodoro")
-  const exit = document.querySelector(".bye-cat")
-  settingPomo.addEventListener('click',e=>{
-    soon.style.setProperty("top","0")    
-  })
-  exit.addEventListener('click',e=>{
-    soon.style.setProperty("top","-100vw")   
-  })
+export default function soonClick() {
+  const settingPomo = document.querySelector(".Setting-pomo");
+  const soon = document.querySelector(".soon-pomodoro");
+  const exit = document.querySelector(".bye-cat");
+  settingPomo.addEventListener("click", (e) => {
+    soon.style.setProperty("top", "0");
+  });
+  exit.addEventListener("click", (e) => {
+    soon.style.setProperty("top", "-100vh");
+  });
 }
